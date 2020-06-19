ee8666d77e3a7ac7408c9c039c56cf3b883da113
Fahim Hoque <fhoque.arnob@gmail.com>
2020-06-19 22:08:43 +0600

    Add files via upload
    Basic linked list creating and print the list using 3 nodes.

---
+++ linkedList.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+
+class node{
+    public:
+        int data;
+        node* next;
+};
+
+// This function will print content of list
+// starting from the given node
+void printList(node* n)
+{
+    while(n != NULL){
+        cout << n->data << " ";
+        n = n->next;
+    }
+}
+
+int main(){
+    node* head = NULL;
+    node* second = NULL;
+    node* third = NULL;
+    
+
+    //allocate 3 nodes in the heap
+    head = new node();
+    second = new node();
+    third = new node();
+
+    head->data = 1; // insert data into head
+    head->next = second; // link first node with the second
+
+    second->data = 55; // insert data into second
+    second->next = third; // link scond node with the third
+
+
+    third->data = 99; // insert data into final noode
+    third->next = NULL; // assign NULL to third node because it's the last node
+
+    printList(head);
+
+    return 0;
+}
